0230e71eca8697226f21b02aff9fd2f190d21534
Alex Stephens <astephens7000@yahoo.com>
2020-02-09 21:19:33 -0500

    Close enough?

--- camera_verification.py
+++ camera_verification.py
@@ -35,14 +35,9 @@ Z = 444.5
 
 # origin transformed from world to camera frame
 origin_world = np.array([0,0,0,1])
-origin_cam = np.dot(origin_world, np.dot(extMatrix, K))/Z
-
-# change extMatrix to extMatrix for camera to world frame
-extRotMatrix = np.concatenate((np.transpose(extRotMatrix), np.zeros((3,1))), axis=1)
-extRotMatrix = np.concatenate((extRotMatrix, np.array([[0,0,0,1]])), axis=0)
-transMatrix = np.concatenate((np.identity(3), -extTransVect), axis=1)
-transMatrix = np.concatenate((transMatrix, np.array([[0,0,0,1]])), axis=0)
-extMatrix = np.dot(extRotMatrix, transMatrix)
+intermed_mat = np.dot(extMatrix, K)/Z
+origin_cam = np.dot(origin_world, intermed_mat)
+inv_intermed_mat = np.linalg.pinv(intermed_mat)
 
 magFocalLength = np.linalg.norm(focalLength)  #Lambda in World Units
 pixelHeight = np.array(focalLength[:] / magFocalLength)  #Pixel Heights Sx and Sy in World Units
@@ -53,13 +48,12 @@ def onMouse(event, r, c, flags, param):  #Grabs mouse input and returns pixel co
         rcCoords = np.array([r,c])
         uvCoords = np.subtract(priPoint, rcCoords)
         uvCoords = np.divide(uvCoords, focalLength)
-        uvCoords = np.append(uvCoords, 1)
-        xyCoords = uvCoords * Z
+        xyCoords = Z * uvCoords
         xyCoords = np.append(xyCoords, 1)
-        xyCoords = xyCoords.reshape((4,1))
-        worldCoords = np.dot(extMatrix, xyCoords)
+        worldCoords = np.dot(xyCoords, inv_intermed_mat)
+        worldCoords = worldCoords / worldCoords[3]  # using final value as scale factor
         print("World Coordinates")
-        print(worldCoords)  # INCORRECT VALUES
+        print(worldCoords)  # close enough
 
 capture = cv2.VideoCapture(0)
 print('Press ESC to Grab Image')
@@ -93,6 +87,3 @@ while not escape:
         print('Pressed esc')
         escape = True
 cv2.destroyAllWindows()
-
-
-
